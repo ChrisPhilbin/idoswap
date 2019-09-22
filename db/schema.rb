70dfdb5f571c6c4a8e52dc90fedc60ee5eb9ae6b
Chris Philbin <chris@chrisphilbin.net>
2019-09-22 15:56:46 -0400

    Setting up relationship between users and items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_194656) do
+ActiveRecord::Schema.define(version: 2019_09_22_195406) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_22_194656) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
