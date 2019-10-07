4044493f0c2b95035c3251f158b2574aa34c773b
Chris Philbin <chris@chrisphilbin.net>
2019-10-07 07:28:26 -0400

    Adding handleSearchChange function to update search results as a user inputs a search term

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { searchProducts } from './searchClient'
 
 function App() {
   return (
---
+++ client/src/products/productSearchForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import searchClient from './client';
+
+const MATCHING_ITEM_LIMIT = 25;
+
+class productSearchForm extends React.Component {
+
+	state = {
+		products: [],
+		searchValue: ''
+	}
+
+	handleSearchChange = (e) => {
+	const value = e.target.value;
+
+	this.setState({
+	  searchValue: value,
+	});
+
+	if (value === '') {
+	  this.setState({
+	    products: []
+	  });
+	} else {
+	  searchClient.search(value, (products) => {
+	    this.setState({
+	      products: products.slice(0, MATCHING_ITEM_LIMIT),
+	    });
+	  });
+	}
+	};
+
+	render() {
+		return (
+			<form>
+				<input
+				className='prompt'
+				type='text'
+				placeholder='Search products...'
+				value={this.state.searchValue}
+				onChange={this.handleSearchChange}
+				/>
+			</form>
+		)
+	}
+}
+
+export default searchResults
