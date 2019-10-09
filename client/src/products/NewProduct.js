91e8ca4bcd34123669f0fa7839548fad9d3d2a8a
Chris Philbin <chris@chrisphilbin.net>
2019-10-08 21:58:47 -0400

    Adding handleFormSubmit function

--- client/src/products/NewProduct.js
+++ client/src/products/NewProduct.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 class NewProduct extends React.Component {
 
+	handleFormSubmit(name, description, price){
+		let body = JSON.stringify({product: {name: name, description: description, price: price} })
 
-
+		fetch('http://localhost:3000/api/products', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: body,
+		}).then((response) => {return response.json()})
+		.then((product)=>{
+			this.addNewProduct(product)
+		})
+	}
 }
 
 export default NewProduct
