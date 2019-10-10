0a81dd19ebc4433549bf086c262c89bec896c3da
Chris Philbin <chris@chrisphilbin.net>
2019-10-10 07:45:48 -0400

    Stubbing out basic getProducts action

---
+++ client/src/products/getProducts.js
@@ -0,0 +1,10 @@
+export const getProducts = () => {
+	return dispatch => {
+		dispatch({type: 'LOADING_PRODUCTS'})
+		return (
+			fetch('/api/products/all')
+			.then(resp => resp.json())
+			.then(products => dispatch({type: 'LOADED_PRODUCTS', payload: products}))
+		)
+	}
+}
